4305faa658e77f2c579a688e73dbc1597fbb64c1
Java <90666661+namekogithub@users.noreply.github.com>
2022-07-25 20:05:48 +0900

    字句解析

--- src/main/java/Interpreter.java
+++ src/main/java/Interpreter.java
@@ -1,9 +1,65 @@
-public class Interpreter {
-    public final static String CODE = "1 + 2 * 3";
+import java.util.ArrayList;
 
+/**
+ * インタプリタ
+ */
+
+public class Interpreter {
+    private static final String CODE = "fn main() {println(\"hello world\")}";
     public static void main(String[] args) {
-        Lexer lexer = new Lexer(CODE);
-        TokenParser parser = new TokenParser(lexer.lex());
-        System.out.println(parser.parse());
+
+    }
+}
+
+/**
+ * 字句解析器(レキサー)
+ */
+
+record Lexer(String code) {
+    private static String tokensString = "";
+    private static ArrayList<String> tokens = new ArrayList<>();
+    public ArrayList<String> lex() throws Exception {
+        boolean stringFlg = false;
+        for (int i = 0; i < code.length(); i++) {
+            if (stringFlg) {
+                if (code.charAt(i) == '\"') {
+                    tokensString.replace("\"", "");
+                    tokens.add(tokensString);
+                    tokens.add("\"");
+                    stringFlg = false;
+                } else {
+                    if (i == code.length() - 1) {
+                        throw new Exception("引用符がありません。");
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            if (code.charAt(i) == ' ' || code.charAt(i) == '\n') {
+                continue;
+            } else if (code.charAt(i) == '(') {
+                tokensString.replace("(", "");
+                tokens.add(tokensString);
+                tokens.add("(");
+            } else if (code.charAt(i) == ')') {
+                tokensString.replace(")", "");
+                tokens.add(tokensString);
+                tokens.add(")");
+            } else if (code.charAt(i) == '{') {
+                tokensString.replace("{", "");
+                tokens.add(tokensString);
+                tokens.add("{");
+            } else if (code.charAt(i) == '}') {
+                tokensString.replace("}", "");
+                tokens.add(tokensString);
+                tokens.add("}");
+            } else if (code.charAt(i) == '\"') {
+                tokensString.replace("\"", "");
+                tokens.add(tokensString);
+                tokens.add("\"");
+                stringFlg = true;
+            }
+        }
+        return new ArrayList<String>();
     }
 }
